1b3d17575923906ec33f1b588c8bc5303598a085
murphy-afk <bardarenoemi@gmail.com>
2026-02-10 18:18:29 +0100

    feat: index query change to allow filter by platform and price

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+
 import express from "express"
 import router from "./Routers/Products.js";
 import productController from "./Controllers/ProductController.js";
 import orderRouter from "./Routers/Orders.js";
 import cors from "cors"
 import routeNotFound from "./MiddleWares/routeNotFound.js";
+import connection from "./database/databaseConnection.js";
 
 const app = express();
 const port = process.env.SERVER_PORT;
@@ -20,7 +22,28 @@ app.use('/images', express.static('public/images'))
 app.use("/retro/api/products", router)
 app.use("/retro/api/orders", orderRouter)
 
-app.get("/retro/api/platforms", productController.platformIndex)
+app.get("/retro/api/platforms", (req, res) => {
+    const sql = `
+    SELECT DISTINCT platforms.name 
+    FROM products
+    INNER JOIN platforms ON products.platform_id = platforms.id
+    ORDER BY platforms.name ASC
+  `;
+
+    connection.query(sql, (err, rows) => {
+        if (err) {
+            console.error("PLATFORMS QUERY ERROR:", err);
+            return res.status(500).json({ success: false, error: err.message });
+        }
+
+        res.json({
+            success: true,
+            results: rows
+        });
+    });
+});
+
+
 
 
 
